e9bdd40ac5bc74558ad7f240ce9bd1bd5b89a28a
Jose Cicero <mr.jociriju@gmail.com>
2025-01-08 18:24:19 -0300

    Add activity sorting by date and time

---
+++ utils.py
@@ -0,0 +1,4 @@
+#  Contém funções auxiliares (como validação de entradas, por exemplo, caso seja necessário no futuro).
+# Funções de validação.
+# Formatação de saída.
+# Cálculos, como estatísticas das atividades.
